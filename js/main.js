37efcd7b172811fbe3dae58b579ab49092804836
honge <hongsun0601@gmail.com>
2022-12-13 22:12:03 +0900

    프로젝트 시작

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+import ipads from '../data/ipads.js'
+import navigations from '../data/navigations.js'
+
+
+
+
+
+
 // 장바구니
 const basketStarterEl = document.querySelector('header .basket-starter');
 
@@ -98,3 +106,60 @@ pauseBtn.addEventListener('click', function () {
     playBtn.classList.remove('hide')
     pauseBtn.classList.add('hide')
 })
+
+const itemsEl = document.querySelector('section.compare .items');
+ipads.forEach(function (ipad) {
+    const itemEl = document.createElement('div')
+    itemEl.classList.add('item')
+
+    let colorList = ''
+    ipad.colors.forEach(function (color) {
+        colorList += `<li style="background-color: ${color};"></li>`
+    })
+
+
+    itemEl.innerHTML = `
+    <div class="thumbnail">
+    <img src="${ipad.thumbnail}" alt="${ipad.name}">
+    </div>
+    <ul class="colors">
+        ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price"> ₩${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>`
+
+    itemsEl.append(itemEl)
+})
+
+
+const navigationsEl = document.querySelector('footer .navigations')
+navigations.forEach(nav => {
+  const mapEl = document.createElement('div')
+  mapEl.classList.add('map')
+
+  let mapList = ''
+  nav.maps.forEach(map => {
+    mapList +=  `<li>
+      <a href="${map.url}">${map.name}</a>
+    </li>`
+  })
+
+  mapEl.innerHTML = /* html */ `
+    <h3>
+      <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
+    </h3>
+    <ul>
+      ${mapList}
+    </ul>
+  `
+
+  navigationsEl.append(mapEl)
+})
+
+
+const thisYearEl = document.querySelector('span.this-year')
+thisYearEl.textContent = new Date().getFullYear()
